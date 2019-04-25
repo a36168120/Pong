0aa44dfe3067e8fc59aec39a842674ea96f27313
Vincent Wang <a36168120@gmail.com>
2019-04-25 16:31:49 -0700

    add Paddle (players), movement, and keys

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,28 +1,55 @@
-import { SVG_NS } from "../settings"
-
+import { SVG_NS, KEYS } from "../settings"
+import Board from './Board'
+import Paddle from './Paddle'
 
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
-
     this.gameElement = document.getElementById(this.element);
+    this.board = new Board(this.width, this.height);
 
+    this.paddleWidth = 8;
+    this.paddleHeight = 56;
+    this.boardGap = 10;
 
-    // Other code goes here...
-  }
+    this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.a,
+      KEYS.z
+    );
+
+    this.player2 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      (this.width - this.boardGap - this.paddleWidth),
+      ((this.height - this.paddleHeight) / 2),
+      KEYS.up,
+      KEYS.down
+    );
+
+  }// End of Constructor...
 
   render() {
     // More code goes here....
     // Copy the code from slids #14...
+    this.gameElement.innerHTML = '';
     let svg = document.createElementNS(SVG_NS, "svg");
     svg.setAttributeNS(null, "width", this.width);
     svg.setAttributeNS(null, "height", this.height);
     svg.setAttributeNS(null, "viewBox", `0 0 ${this.width} ${this.height}`);
     this.gameElement.appendChild(svg);
 
-    this.gameElement.innerHTML = '';
+    this.board.render(svg);
+    this.player1.render(svg);
+    this.player2.render(svg);
+
 
   }
 }
